0af36234179e4616d859b7147bb04cac07b99c51
vasu9893 <64803883+vasu9893@users.noreply.github.com>
2024-12-26 13:27:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:awesome_portfolio/providers/current_state.dart';
+import 'package:awesome_portfolio/providers/theme_provider.dart';
+import 'package:awesome_portfolio/screen/homescreen/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const Portfolio());
+}
+
+class Portfolio extends StatelessWidget {
+  const Portfolio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => CurrentState()),
+      ],
+      child: const MaterialApp(
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
